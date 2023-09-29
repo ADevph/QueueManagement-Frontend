d1ddc60a62c5e12060b514148649654c6a5b4b84
Sardar Piash <piashm66@gmail.com>
2023-09-29 21:38:35 +0600

    Registration Done

--- pages/registration.js
+++ pages/registration.js
@@ -1,180 +1,269 @@
-
 import Layout from "./Layout/layout";
 import Title from "./Layout/title";
 import Link from "next/link";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/router";
 
-// import { useState, useEffect } from 'react';
-// import axios from 'axios';
+export default function RegisterPage() {
+  const [firstName, setFirstName] = useState("");
+  const [firstNameError, setFirstNameError] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [lastNameError, setLastNameError] = useState("");
+  const [phone, setPhone] = useState("");
+  const [phoneError, setPhoneError] = useState("");
+  const [gender, setGender] = useState("");
+  const [genderError, setGenderError] = useState("");
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+  const { handleSubmit } = useForm();
+  const[loading,setLoading]=useState(false);
 
-export default function RegisterPage () {
-
-//   const [fullName, setFullName] = useState('');
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
-//   const [confirmPassword, setConfirmPassword] = useState('');
-//   const [phone, setPhone] = useState('');
-//   const [file, setFile] = useState(null);
-//   const [error, setError] = useState('');
-
-
-
+  const onSubmit = async () => {
+    setLoading(true);
+    setFirstNameError("");
+    setLastNameError("");
+    setGenderError("");
+    setEmailError("");
+    setPhoneError("");
+    setConfirmPasswordError("");
+    setPasswordError("");
   
-//   useEffect(() => {
-//     setFullName('');
-//     setEmail('');
-//     setPassword('');
-//     setConfirmPassword('');
-//     setPhone('');
-//     setFile(null);
-//     setError('');
-//   }, []);
+    if(!firstName){
+      setFirstNameError("Enter Your First Name First");
+      setLoading(false);
+      return;
+    }
+  //   const firstNamePattern = /^[A-Za-z0-9 ]{1,20}$/;
+  // if (!firstNamePattern.test(firstName)) {
+  //   setFirstNameError("First name can only contain letters, numbers, and spaces. Maximum length is 20 characters.");
+  //   setLoading(false);
+  //   return;
+  // }
+
+  if(!lastName){
+    setLastNameError("Enter Your Last Name");
+    setLoading(false);
+    return;
+  }
+  if(!phone){
+    setPhoneError("Enter Your Phone Number");
+    setLoading(false);
+    return;
+  }
+  if(!gender){
+    setGenderError("Enter Your Gender");
+    setLoading(false);
+    return;
+  }
+  if(!email){
+    setEmailError("Enter Your Email");
+    setLoading(false);
+    return;
+  }
+  if(!password){
+    setPasswordError("Enter Your Password");
+    setLoading(false);
+    return;
+  }
+  if(!confirmPassword){
+    setConfirmPasswordError("Enter Confirm Password");
+    setLoading(false);
+    return;
+  }
+  if(password !== confirmPassword){
+    setConfirmPasswordError("Confirm Password Should be same as Password");
+    setLoading(false);
+    return;
+  }
+    try{
+      //const response = await.axios.post......................
 
 
-//   const handleChangeFullName = (e) => {
-//     setFullName(e.target.value);
-//   };
-
-//   const handleChangeEmail = (e) => {
-//     setEmail(e.target.value);
-//   };
-
-//   const handleChangePassword = (e) => {
-//     setPassword(e.target.value);
-//   };
-
-//   const handleChangeConfirmPassword = (e) => {
-//     setConfirmPassword(e.target.value);
-//   };
-
-//   const handleChangePhone = (e) => {
-//     setPhone(e.target.value);
-//   };
-
-
-//   const handleChangeFile = (e) => {
-//     setFile(e.target.files[0]);
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-    
-//     if (!fullName || !email || !password || !confirmPassword || !phone || !file) {
-//       console.log(fullName, email, password, confirmPassword, phone, file);
-//       setError('All fields are required');
-//     } else if (password !== confirmPassword) {
-//       setError('Passwords do not match');
-//     } else {
-     
-//     try {
-//       postData()
-//       setError("user created successfully");
-//     } catch (e) {
-//       setError(e);
-//     }
-//       setFullName('');
-//       setEmail('');
-//       setPassword('');
-//       setConfirmPassword('');
-//       setPhone('');
-//       setFile(null);
-//       setError('');
-//     }
-//   };
-
-//   async function postData() {
-//    try {
-//      const formData = new FormData();
-//      formData.append('name', fullName);
-//      formData.append('email', email);
-//      formData.append('password', password);
-//      formData.append('phone', phone);
-//      formData.append('image', document.querySelector('#myfile').files[0]);
-//     console.log(formData);
-//      const response = await axios.post(process.env.NEXT_PUBLIC_API_ENDPOINT + '/student/signup/', formData, {
-//       headers: {
-//         'Content-Type': 'multipart/form-data'
-//     }
-//      });
-    
-
-//      const data = response.data;
-//      console.log(data);
-//      } catch (error) {
-//      console.error(error);
-//      }
-//     }
+      console.log("Success");
+    }catch(error){
+      setError(error);
+      setLoading(false);
+      return;
+    }
+  };
 
   return (
     <>
+      <Title page="Registration" />
 
-     <Title page="Registration"> </Title>
+      <Layout>
+        <div className="flex items-center justify-center h-screen">
+          <div className="relative flex flex-col items-center justify-center rounded-xl bg-white shadow-md p-8 w-128">
+            <h4 className="block font-sans text-2xl font-bold leading-snug tracking-normal text-blue-gray-900 antialiased">
+              Sign Up
+            </h4>
+            <p className="mt-1 mb-6 block font-sans text-base font-normal leading-relaxed text-gray-700 antialiased">
+              Enter your details to register.
+            </p>
 
-  <Layout>
+            <form onSubmit={handleSubmit(onSubmit)} className="w-full">
+              <div className="mb-4">
+                <label className="font-semibold text-xl text-black">
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  name="firstName"
+                  placeholder="Enter your First name"
+                  className="input-field bg-blue-100 p-1 rounded-lg"
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+                {firstNameError && (
+                  <p className="text-red-500">{firstNameError}</p>
+                )}
+              </div>
+              <div className="mb-4">
+                <label className="font-semibold text-xl text-black">
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  name="lastName"
+                  placeholder="Enter your Last name"
+                  className="input-field bg-blue-100 p-1 rounded-lg"
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+                {lastNameError && (
+                  <p className="text-red-500">{lastNameError}</p>
+                )}
+              </div>
 
-  <div className="flex items-center justify-center h-screen">
-  <div className="relative flex flex-col items-center justify-center rounded-xl bg-white shadow-md p-8 w-128">
-    <h4 className="block font-sans text-2xl font-bold leading-snug tracking-normal text-blue-gray-900 antialiased">
-    Sign Up
-  </h4>
-  <p className="mt-1 mb-6 block font-sans text-base font-normal leading-relaxed text-gray-700 antialiased">
-    Enter your details to register.
-  </p>
+              <div className="mb-4">
+                <label className="font-semibold text-xl text-black">
+                  Phone: +88
+                </label>
+                <input
+                  type="tel"
+                  name="phone"
+                  placeholder=""
+                  className="input-field bg-blue-100 p-1 rounded-lg"
+                  onChange={(e) => setPhone(e.target.value)}
+                  pattern="^\01\d{8,}$"
+                  title="Enter a valid Bangladeshi phone number starting with +88"
+                />
+                {phoneError && (
+                  <p className="text-red-500">{phoneError}</p>
+                )}
+              </div>
 
+              <div className="mb-4">
+                <label className="font-semibold text-xl text-black">
+                  Gender
+                </label>
+                <div className="flex items-center space-x-4">
+                  <div className="flex items-center">
+                    <input
+                      type="radio"
+                      id="male"
+                      name="gender"
+                      value="male"
+                      className="mr-2 text-blue-500"
+                      onChange={(e) => setGender(e.target.value)}
+                    />
+                    <label htmlFor="male" className="text-xl text-black">
+                      Male
+                    </label>
+                  </div>
+                  <div className="flex items-center">
+                    <input
+                      type="radio"
+                      id="female"
+                      name="gender"
+                      value="female"
+                      className="mr-2 text-blue-500"
+                      onChange={(e) => setGender(e.target.value)}
+                    />
+                    <label htmlFor="female" className="text-xl text-black">
+                      Female
+                    </label>
+                  </div>
+                </div>
+                {genderError && (
+                  <p className="text-red-500">{genderError}</p>
+                )}
+              </div>
 
-    <div className="mb-4 w-full">
-      <label className="font-semibold text-xl text-black">First Name</label>
-      <input type="text" name="fullName" placeholder="Enter your First name" className="input-field bg-blue-100 ml-8 p-1 rounded-lg" />
-    </div>
-    <div className="mb-4 w-full">
-      <label className="font-semibold text-xl text-black">Last Name</label>
-      <input type="text" name="fullName" placeholder="Enter your Last name" className="input-field bg-blue-100 ml-8 p-1 rounded-lg" />
-    </div>
+              <div className="mb-4">
+                <label className="font-semibold text-xl text-black">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="john.doe@example.com"
+                  className="input-field bg-blue-100 p-1 rounded-lg"
+                  onChange={(e) => setEmail(e.target.value)}
+                  pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+                  title="Enter a valid email address (e.g., john.doe@example.com)"
+                />
+                {emailError && (
+                  <p className="text-red-500">{emailError}</p>
+                )}
+              </div>
 
+              <div className="mb-4">
+                <label className="font-semibold text-xl text-black">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Enter Your Password"
+                  className="input-field bg-blue-100 p-1 rounded-lg"
+                  onChange={(e) => setPassword(e.target.value)}
+                  pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+                  title="Password must be at least 8 characters long and contain at least one letter and one number"              
+                />
+                {passwordError && (
+                  <p className="text-red-500">{passwordError}</p>
+                )}
+              </div>
 
-<div className="mb-4 w-full flex items-center">
-  <div className="text-xl text-black font-semibold mr-2">Phone : +88</div>
-  <input type="tel" name="phone" placeholder="" className="input-field bg-blue-100 ml-2 p-1 rounded-lg" />
-</div>
+              <div className="mb-4">
+                <label className="font-semibold text-xl text-black">
+                  Confirm Password
+                </label>
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  placeholder=" Confirm Your Password"
+                  className="input-field bg-blue-100 p-1 rounded-lg"
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                {confirmPasswordError && (
+                  <p className="text-red-500">{confirmPasswordError}</p>
+                )}
+              </div>
 
-<div className="mb-4 w-full flex items-center">
-  <label className="font-semibold text-xl text-black mr-10">Gender</label>
-  <div className="flex items-center space-x-8">
-    <label className="flex items-center">
-      <input type="radio" name="gender" value="male" className="mr-2" />
-      Male
-    </label>
-    <label className="flex items-center">
-      <input type="radio" name="gender" value="female" className="mr-2" />
-      Female
-    </label>
-  </div>
-</div>
+              <button
+                className="btn btn-outline btn-secondary"
+                type="submit"
+              >
+                Register
+              </button>
+              {error && (<p className="text-red-500">{error}</p>)}
 
-
-    <div className="mb-4 w-full">
-      <label className="font-semibold text-xl text-black">Email</label>
-      <input type="email" name="email" placeholder="john.doe@example.com" className="input-field bg-blue-100 ml-14 p-1 rounded-lg" />
-    </div>
-    <div className="mb-4 w-full">
-      <label className="font-semibold text-xl text-black">Password</label>
-      <input type="password" name="password" placeholder="Enter Your Password" className="input-field bg-blue-100 ml-5 p-1 rounded-lg" />
-    </div>
-    <div className="mb-4 w-full">
-      <label className="font-semibold text-xl text-black">Confirm Password</label>
-      <input type="password" name="confirmPassword" placeholder=" Confirm Your Password" className="input-field bg-blue-100 ml-2 p-1 rounded-lg" />
-    </div>
-  
-    <button className="btn btn-outline btn-secondary" type="submit">Register</button>
-    <p className="mt-4 text-center font-semibold text-xl text-black">
-      Already have an account?
-      <Link className="text-pink-500 hover:text-blue-700" href="loginform" > Sign In </Link>
-    </p>
-  </div>
-</div>
-
-
+              <p className="mt-4 text-center font-semibold text-xl text-black">
+                Already have an account?
+                <a href="/loginform" className="text-pink-500 hover:text-blue-700">Sign In
+                </a>
+              </p>
+            </form>
+          </div>
+        </div>
       </Layout>
     </>
   );
-};
-
+}
