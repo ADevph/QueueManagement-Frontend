6a08093bb044481ee5451bacca67cca1f51b4fbe
01HB <bspaceship3@gmail.com>
2023-10-01 02:15:54 +0600

    -.-

--- pages/Layout/layout.js
+++ pages/Layout/layout.js
@@ -1,9 +1,21 @@
+import Footer from './footer'
+import PatientHeader from './patientheader'
 
-export default function Layout(props) {
 
-  return (
-    <>
+export default function Layout (
+    {children}
+){
+    return (
+       
+        <>
+
+<PatientHeader> </PatientHeader>
+
+
+  {children}
+
+<Footer/>
+        </>
       
-    </>
-  );
+    )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,44 @@
 
 
 
+import Link from "next/link";
+import dynamic from "next/dynamic";
+const Layout = dynamic(() => import('./Layout/layout'), {
+  ssr: false,
+})
+const Title = dynamic(() => import('./Layout/title'), {
+  ssr: false,
+})
+
+
 
 export default function Home() {
+
   return (
     <>
+
+      <Title page="Home"> </Title>
+      <Layout>
+
+        <div className="hover:bg-cyan-200  text-4xl text-center text-black bg-slate-200 p-2">
+          <h1> Welcome! </h1>
+        </div>
+
+        <div className="mt-4 flex items-center justify-center">
+
+          <img src="../q.png" className=""></img>
+
+        </div>
+        <br></br>
+        <br></br>
+
+
+
+
+
+      </Layout>
+
+
     </>
 
   )
