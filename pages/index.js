9115080601c666f4ad2346dc0e5d75f7b6df2625
01HB <bspaceship3@gmail.com>
2023-10-01 01:13:46 +0600

    grsrryyr

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,10 @@
-import Image from "next/image";
-import Link from "next/link";
-import Layout from "./layout/layout";
+
 
 
 
 export default function Home() {
   return (
     <>
-      <Layout title="Home - Queue Management">
-        <section className="flex justify-center">
-          <div className="flex flex-col justify-center items-center w-full max-w-[1600px] min-h-screen pt-[75px] pb-[50px]">
-            <div className="text-4xl text-center text-black p-2">
-              <h1> Welcome Here! </h1>
-            </div>
-
-            <div className="mt-4 flex items-center justify-center w-full">
-              <Image src="/q.png" alt="image" width={200} height={200} className="object-cover origin-center" />
-            </div>
-          </div>
-        </section>
-      </Layout>
-
     </>
 
   )
